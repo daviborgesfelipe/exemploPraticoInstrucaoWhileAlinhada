1899b826e46d17ce84a24802441df81618e0206a
Davi Borges <davifelipe_300@hotmail.com>
2022-02-04 16:53:16 -0300

    Contador Externo e Contador Interno

---
+++ Program.cs
@@ -0,0 +1,28 @@
+﻿/*Tres interacoes do laço externo, e cinco interacao do laço interno*/
+using System;
+
+namespace exemploPraticoInstrucaoWhileAlinhada
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {   
+            int contadorExterno = 0;
+            int contadorInterno = 0;
+
+            while(contadorExterno < 3)
+            {   
+                Console.WriteLine($"\n##### Laço externo: {contadorExterno} #####");
+                
+                while (contadorInterno < 5)
+                {
+                    Console.WriteLine($"Laço interno: {contadorInterno}");
+                    contadorInterno = contadorInterno + 1;
+                }
+
+                contadorExterno = contadorExterno + 1;
+                contadorInterno = 0;
+            }
+        }
+    }
+}
